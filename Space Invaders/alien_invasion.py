7165c714119ebc5b184c1b12b90e60324c75a507
ulemon <vegas7546@gmail.com>
2021-12-05 20:02:48 +0300

    Add space ship and mario

--- Space Invaders/alien_invasion.py
+++ Space Invaders/alien_invasion.py
@@ -1,4 +1,7 @@
 import sys
+from settings import Settings
+from ship import Ship
+from mario import Mario
 import pygame
 
 class AlienInvasion:
@@ -7,22 +10,30 @@ class AlienInvasion:
     def __init__(self):
         """Initialize the game, and create game resources."""
         pygame.init()
-
-        self.screen = pygame.display.set_mode((1280, 720))
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode(self.settings.screen_size)
         pygame.display.set_caption("Alien Invasion")
 
-        # Set the bg color.
-        self.bg_color = (23, 150,14)
+        self.ship = Ship(self)
+        self.mario = Mario(self)
 
     def run_game(self):
         """Start the main loop for the game."""
         while True:
-            # Watch for keyboard and mouse events.
+            self._check_events()
+            self._update_screen()
+    
+    def _check_events(self):
+        # Watch for keyboard and mouse events.
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-            # Redraw the screen during each pass through the loop.
-            self.screen.fill(self.bg_color)
+
+    def _update_screen(self):
+         # Redraw the screen during each pass through the loop.
+            self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
+            self.mario.blitme()
             # Make the most recently drawn screen visible.
             pygame.display.flip()
 
---
+++ Space Invaders/settings.py
@@ -0,0 +1,8 @@
+class Settings:
+    """A class to store all settings for Alien Invasion."""
+
+    def __init__(self):
+        """Initialize the game's settings."""
+        # Screen settings
+        self.screen_size = (1280, 720)
+        self.bg_color = (0, 0, 0)
